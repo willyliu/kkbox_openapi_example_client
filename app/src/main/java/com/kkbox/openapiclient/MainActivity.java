51d07d65ff3a79e29bd6e90c5edc9ccba567a596
Sharon Yang <sharonyang@kkbox.com>
2017-10-11 17:47:09 +0800

    Completes the example app.

--- app/src/main/java/com/kkbox/openapiclient/MainActivity.java
+++ app/src/main/java/com/kkbox/openapiclient/MainActivity.java
@@ -3,29 +3,30 @@ package com.kkbox.openapiclient;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.renderscript.ScriptIntrinsic;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.AbsListView;
 import android.widget.ListView;
 import android.widget.SearchView;
-import android.widget.TextView;
 
 import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
 import com.kkbox.openapi.ClientInfo;
 import com.kkbox.openapi.HttpClient;
 import com.kkbox.openapi.SearchFetcher;
 import com.kkbox.openapi.TokenFetcher;
-import com.kkbox.openapiclient.TrackInfo;
 import com.koushikdutta.ion.future.ResponseFuture;
 
 import java.util.ArrayList;
-import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
-    private boolean loading;
+    boolean loading;
+    boolean hasMore;
+    HttpClient client;
+    SearchFetcher searchFetcher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,11 +36,11 @@ public class MainActivity extends AppCompatActivity {
         // get token
         Context appContext = getApplicationContext();
         TokenFetcher fetcher = new TokenFetcher(ClientInfo.CLIENT_ID, ClientInfo.CLIENT_SECRET, appContext);
-        ResponseFuture<JsonObject> responseFurure = fetcher.fetchAccessTokenWithClientCredentials();
+        ResponseFuture<JsonObject> responseFuture = fetcher.fetchAccessTokenWithClientCredentials();
         try {
-            JsonObject response = responseFurure.get();
+            JsonObject response = responseFuture.get();
             String token = response.get("access_token").getAsString();
-            HttpClient client = new HttpClient(token, appContext);
+            client = new HttpClient(token, appContext);
 
             //set search view listener
             final SearchView searchView = findViewById(R.id.search_bar);
@@ -56,45 +57,153 @@ public class MainActivity extends AppCompatActivity {
                 }
             };
             searchView.setOnQueryTextListener(queryTextListener);
-
-            final ListView listView = findViewById(R.id.listView);
-
-
-//            // get search result
-//            SearchFetcher searchFetcher = new SearchFetcher(client, "TW");
-//            ResponseFuture<JsonObject> searchResult = searchFetcher.setSearchCriteria("love", "track").fetchSearchResult(15, 0);
-//
-//            // parse search result
-//            JsonObject searchResponse = searchResult.get();
-//            JsonObject tracks = searchResponse.get("tracks").getAsJsonObject();
-//            JsonArray data = tracks.get("data").getAsJsonArray();
-//            StringBuilder songNames = new StringBuilder();
-//            for (int i = 0; i < data.size(); i++) {
-//                JsonObject track = data.get(i).getAsJsonObject();
-//                JsonPrimitive namePrimitive = track.getAsJsonPrimitive("name");
-//                String name = namePrimitive.getAsString();
-//                String line = String.format(Locale.getDefault(), "%d. %s\n", i + 1, name);
-//                songNames.append(line);
-//            }
-//
-//            // displays all songs' names
-//            TextView responseTextView = findViewById(R.id.responseTextView);
-//            responseTextView.setText(songNames.toString());
         }
         catch (Exception e) {
             Log.d("error", e.toString());
         }
+
+        final ListView listView = findViewById(R.id.listView);
+        TrackListAdapter adapter = new TrackListAdapter(this);
+        if(listView != null) {
+            listView.setAdapter(adapter);
+        }
+        listView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView var1, int var2) {}
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                if (firstVisibleItem + visibleItemCount == totalItemCount && totalItemCount != 0){
+                    if (!loading) {
+                        fetchMoreData();
+                    }
+                }
+            }
+        });
     }
 
-    private final void generateData(final String q) {
+    private void generateData(final String q) {
         loading = true;
         AsyncTask.execute(new Runnable() {
 
             @Override
             public void run() {
+
                 final ArrayList<TrackInfo> result = new ArrayList<>();
+                searchFetcher = new SearchFetcher(client, "TW");
+
+                try {
+                    JsonObject searchResult = searchFetcher.setSearchCriteria(q, "track").fetchSearchResult(15, 0).get();
+                    parseSearchResult(searchResult, result);
+
+                    JsonObject response = searchResult.getAsJsonObject();
+                    JsonObject track = response.getAsJsonObject("tracks");
+                    if (track != null && !track.isJsonNull()) {
+                        JsonObject paging = track.get("paging").getAsJsonObject();
+                        JsonElement nextUri = paging.get("next");
+
+                        hasMore = (nextUri != null)? !nextUri.isJsonNull() : false;
+                    }
+
+                    MainActivity.this.runOnUiThread(new Runnable(){
+
+                        @Override
+                        public void run() {
+                            final ListView listView = MainActivity.this.findViewById(R.id.listView);
+                            TrackListAdapter trackListAdapter = (TrackListAdapter)listView.getAdapter();
+                            trackListAdapter.items = result;
+                            trackListAdapter.notifyDataSetChanged();
+                            loading = false;
+                        }
+                    });
+                }
+                catch (Exception e) {
+                    Log.d("error", e.toString());
+                }
+            }
+        });
+    }
+
+    private void fetchMoreData() {
+        if (!hasMore) {
+            return;
+        }
+        loading = true;
+        final ListView listView = MainActivity.this.findViewById(R.id.listView);
+        final TrackListAdapter trackListAdapter = (TrackListAdapter)listView.getAdapter();
+        final int offset = trackListAdapter.items.size();
+
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                final ArrayList<TrackInfo> result = new ArrayList<>();
+
+                try {
+                    JsonObject searchResult = searchFetcher.fetchSearchResult(15, offset).get();
+                    parseSearchResult(searchResult, result);
+
+                    JsonObject response = searchResult.getAsJsonObject();
+                    JsonObject track = response.get("tracks").getAsJsonObject();
+                    if (track != null && !track.isJsonNull()) {
+                        JsonObject paging = track.get("paging").getAsJsonObject();
+                        JsonElement nextUri = paging.get("next");
+
+                        hasMore = (nextUri != null)? !nextUri.isJsonNull() : false;
+                    }
+
+                    MainActivity.this.runOnUiThread(new Runnable(){
+
+                        @Override
+                        public void run() {
+                            trackListAdapter.items.addAll(result);
+                            trackListAdapter.notifyDataSetChanged();
+                            loading = false;
+                        }
+                    });
+                }
+                catch (Exception e) {
+                    Log.d("error", e.toString());
+                }
 
             }
         });
+
+    }
+
+    private void parseSearchResult(JsonObject searchResult, ArrayList<TrackInfo> result) {
+        if (searchResult == null) {
+            return;
+        }
+        if (searchResult.isJsonNull()) {
+            return;
+        }
+        try {
+            JsonObject searchResponse = searchResult;
+            JsonObject tracks = searchResponse.getAsJsonObject("tracks");
+            if (tracks == null || tracks.isJsonNull()) {
+                return;
+            }
+            JsonArray data = tracks.get("data").getAsJsonArray();
+            for (int i = 0; i < data.size(); i++) {
+                JsonObject track = data.get(i).getAsJsonObject();
+                JsonPrimitive trackPrimitive = track.getAsJsonPrimitive("name");
+                String trackName = trackPrimitive.getAsString();
+
+                JsonObject album = track.getAsJsonObject("album");
+                JsonObject images = album.get("images").getAsJsonArray().get(0).getAsJsonObject();
+                JsonPrimitive albumPrimitive = images.getAsJsonPrimitive("url");
+                String albumImage = albumPrimitive.getAsString();
+
+                JsonObject artist = album.getAsJsonObject("artist");
+                JsonPrimitive artistPrimitive = artist.getAsJsonPrimitive("name");
+                String artistName = artistPrimitive.getAsString();
+
+                TrackInfo trackInfo = new TrackInfo(trackName, artistName, albumImage);
+                result.add(trackInfo);
+            }
+        }
+        catch (Exception e) {
+            Log.d("error", e.toString());
+        }
     }
 }
--- app/src/main/java/com/kkbox/openapiclient/TrackListAdapter.java
+++ app/src/main/java/com/kkbox/openapiclient/TrackListAdapter.java
@@ -1,11 +1,20 @@
 package com.kkbox.openapiclient;
 
 import android.app.Activity;
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import java.util.ArrayList;
 import com.kkbox.openapiclient.TrackInfo;
+import com.koushikdutta.ion.Ion;
+
+import org.w3c.dom.Text;
 
 /**
  * Created by sharonyang on 2017/10/3.
@@ -13,26 +22,76 @@ import com.kkbox.openapiclient.TrackInfo;
 
 public class TrackListAdapter extends BaseAdapter{
 
-    private ArrayList<TrackInfo> items;
-    private Activity activity;
+    ArrayList<TrackInfo> items;
+    Activity activity;
+
+    public TrackListAdapter(Activity activity) {
+        this.activity = activity;
+        this.items = new ArrayList();
+    }
+
+    private static final class ViewHolder {
+        private TextView txtTrackName;
+        private TextView txtArtist;
+        private ImageView imgAlbum;
+
+        public final TextView getTxtTrackName() {return this.txtTrackName;}
+        public final void setTxtTrackName(TextView trackName) {this.txtTrackName = trackName; }
+        public final TextView getTxtArtist() {return  this.txtArtist;}
+        public final void setTxtArtist(TextView artist) {this.txtArtist = artist;}
+        public final ImageView getImgAlbum() {return this.imgAlbum;}
+        public final void setImgAlbum(ImageView album) {this.imgAlbum = album;}
+
+        public ViewHolder(View row) {
+            this.txtTrackName = (TextView)(row != null?row.findViewById(R.id.txtTrackName):null);
+            this.txtArtist = (TextView)(row != null?row.findViewById(R.id.txtArtist):null);
+            this.imgAlbum = (ImageView)(row != null?row.findViewById(R.id.imgAlbum):null);
+        }
+    }
 
     @Override
     public int getCount() {
-        return 0;
+        return this.items.size();
     }
 
     @Override
     public Object getItem(int i) {
-        return null;
+        return this.items;
     }
 
     @Override
     public long getItemId(int i) {
-        return 0;
+        return (long) i;
     }
 
     @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view;
+        TrackListAdapter.ViewHolder viewHolder;
+        if(convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) this.activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.track_list_row, null);
+            viewHolder = new TrackListAdapter.ViewHolder(view);
+            if(view != null) {
+                view.setTag(viewHolder);
+            }
+        } else {
+            view = convertView;
+            viewHolder = (TrackListAdapter.ViewHolder) view.getTag();
+        }
+
+        TrackInfo trackInfo = this.items.get(position);
+        TextView text = viewHolder.getTxtTrackName();
+        if(text != null) {
+            text.setText(trackInfo.name);
+        }
+
+        text = viewHolder.getTxtArtist();
+        if(text != null) {
+            text.setText(trackInfo.artist);
+        }
+
+        Ion.with(viewHolder.getImgAlbum()).load(trackInfo.albumImage);
+        return view;
     }
 }
